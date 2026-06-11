7163c2c1ca95ff69dfc30ddddb39b6a48650496f
yanmell0 <yanmellodn@gmail.com>
2026-06-11 12:29:37 -0300

    Remove: autenticacao removida

--- push_github.bat
+++ push_github.bat
@@ -1,14 +1,14 @@
 @echo off
 cd /d "%~dp0"
-echo Iniciando envio para o GitHub...
+echo Enviando para o GitHub...
 
-git init
-git remote remove origin 2>nul
-git remote add origin https://github.com/yanmell0/cfopm2026.1.com.git
-git add index.html
-git commit -m "Dashboard CFO PM 2026.2"
-git branch -M main
-git push -u origin main
+del /f "%~dp0.git\HEAD.lock" 2>nul
+git rm --cached --ignore-unmatch middleware.js
+del /f middleware.js 2>nul
+git add push_github.bat
+git commit -m "Remove: autenticacao removida"
+git push origin main
+git push origin main:master
 
 echo.
 echo Concluido! Acesse: https://yanmell0.github.io/cfopm2026.1.com
