b88b69253a70ee5cc0c90faae7e82752be20ae7a
yanmell0 <yanmellodn@gmail.com>
2026-06-11 12:38:07 -0300

    Fix: adiciona package.json e corrige middleware

--- push_github.bat
+++ push_github.bat
@@ -3,8 +3,8 @@ cd /d "%~dp0"
 echo Enviando para o GitHub...
 
 del /f "%~dp0.git\HEAD.lock" 2>nul
-git add middleware.js push_github.bat
-git commit -m "Fix: middleware com autenticacao corrigida"
+git add middleware.js push_github.bat package.json
+git commit -m "Fix: adiciona package.json e corrige middleware"
 git push origin main
 git push origin main:master
 
